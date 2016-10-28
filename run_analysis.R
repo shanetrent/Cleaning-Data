d5b6ea27de278b3d0869ebf27b8b6810bd16a374
Shane Trent <shane.trent@outlook.com>
2016-10-28 19:53:07 -0400

    Updated run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -2,7 +2,7 @@ library(dplyr)
 library(tidyr)
 
 
-# Merges the training and the test sets to create one data set.
+#Reading in Files into Variable Names.
 
 ActTest <- read.table("./test/X_test.txt")
 DataTest <- read.table("./test/Y_test.txt")
@@ -13,37 +13,34 @@ SubTrain <- read.table("./train/subject_train.txt")
 Act <- read.table("activity_labels.txt")
 Feat <- read.table("features.txt")
 
+#Combining Subject,Activity and Training data into their own Variables.
+
 SubAll <- rbind(SubTrain, SubTest)
 ActAll <- rbind(ActTrain, ActTest)
 DataAll <- rbind(DataTrain, DataTest)
+
+#ASsigning Column Names to each Variable set.
 
 colnames(Feat) <- c("FeatNum","FeatName")
 colnames(Act) <- c("ActNum","ActName")
 colnames(DataAll) <- ("ActivityType")
 colnames(SubAll) <- ("Subject")
-AllSubAct <- cbind(SubAll, DataAll )
+colnames(ActAll) <- Feat$FeatName
+
+#Assigning Activity Names to each related category in the DataAll set of variables.
 
 DataAll[,1] = Act[DataAll[,1],2]
 
+#Combine Subject and Training Data into one set.
 
+AllSubAct <- cbind(SubAll, DataAll )
 
+#Combine above set with Activity to create just one dataset with Subject and ActivityType Labels.
 
 AllData <- cbind(AllSubAct, ActAll)
 
-#Extracts only the measurements on the mean and standard deviation for each measurement.
-
-
-
-## Keep only data we want, and name it human readable
-
-
-
-#Uses descriptive activity names to name the activities in the data set
+#AllData should be Tidy Dataset with readable headers and merged data.
 
 
 
 
-#Appropriately labels the data set with descriptive variable names.
-
-
-#From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
